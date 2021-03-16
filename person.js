3049f80943263239a4a9d0bd70a56da1b4763ba1
leilali0 <46583372+leilali0@users.noreply.github.com>
2021-03-16 15:41:02 -0700

    new classes

---
+++ person.js
@@ -0,0 +1,7 @@
+class Person {
+	constructor(xPos, yPos, size){
+		this.x = xPos;
+		this.y = yPos;
+		this.s = size;
+	}
+}	
---
+++ sketch.js
@@ -0,0 +1,10 @@
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0);
+  rect(30, 20, 55, 55);
+}
