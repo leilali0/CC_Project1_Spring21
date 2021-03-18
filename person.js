fabc9df642cf7aa1269f27b95f4c00c7971eca60
leilali0 <46583372+leilali0@users.noreply.github.com>
2021-03-18 12:57:22 -0700

    person's face complete

--- person.js
+++ person.js
@@ -7,27 +7,45 @@ class Person {
 		this.m = mood; //boolean true = good mood, and false = bad mood
 	}
 
-	/*display(){
-		makeEye();
-	}*/
+	display(){
+		this.makeEye();
+		this.makeMouse();
+		this.makeHead();
+	}
 
-	/*changeMood(){
-		if(this.m == 1){
-			this.m == 2;
+	changeMood(){
+		if(this.m ){
+			this.m == false;
 			makeEye();
 			makeMouse();
 		} else {
-			this.m == 1;
+			this.m == true;
 			makeEye();
 			makeMouse();
 		}
-	}*/
+	}
 
 	makeEye() {
-		//if(this.m == 1){
-		fill(0);
-		rect(100,100,100,100);
-		circle(200, 200, 100, 100);
-		//}
+		if(this.m){
+			fill(0);
+			ellipse(this.x - 50, this.y - 10, 20, 20);
+		} else {
+
+		}
+	}
+
+	makeMouse(){
+		if(this.m){
+			stroke(0);
+			noFill();
+			arc(this.x - 70, this.y + 40, 80, 40, 0, HALF_PI);
+		}
+	}
+
+	makeHead(){
+		noFill();
+		strokeWeight(4);
+		stroke(0);
+		ellipse(this.x, this.y, 200, 200);
 	}
 }	
--- sketch.js
+++ sketch.js
@@ -3,12 +3,12 @@ let ming;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  ming = new Person(100, 100, 1, 1);
+  ming = new Person(300, 300, 200, 200);
 }
 
 function draw() {
   background(255);
   fill(0);
-  //rect(100,100,100,100);
-  ming.makeEye();
+
+  ming.display();
 }
